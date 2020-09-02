bf8ec594447bd86188e18564a978609de91fbb31
westerovs <westerovs@mail.ru>
2020-09-02 23:30:33 +0300

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
   <div id="app">
-
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
-<style>
+<script>
+  import Navbar from './components/Navbar';
 
+  export default {
+    components: {
+      Navbar
+    }
+  }
+</script>
+
+<style>
+  *,
+  ::before,
+  ::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: rgb(119, 114, 167);
+    color: white;
+    font-family: sans-serif;
+  }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+    <nav class="nav">
+        <ul>
+            <router-link to="/" tag="li" exact active-class="active">
+                <a href="#">Tasks</a>
+            </router-link>
+            <router-link to="/one" tag="li" active-class="active">
+                <a href="#">One</a>
+            </router-link>
+            <router-link to="/two" tag="li" active-class="active">
+                <a href="#">Two</a>
+            </router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .nav {
+        width: 100%;
+        height: 100px;
+        background-color: blue;
+    }
+
+    ul {
+        padding: 10px;
+        display: grid;
+        grid-template-columns:
+            min-content
+            min-content
+            min-content
+        ;
+    }
+
+    li {
+        border: 1px solid white;
+        list-style-type: none;
+        transition: 0.3s;
+    }
+
+    li:hover {
+        background-color: black;
+    }
+
+    .active {
+        background-color: blueviolet;
+    }
+
+    a {
+        display: block;
+        padding: 10px;
+        color: white;
+        text-decoration: none;
+    }
+</style>
